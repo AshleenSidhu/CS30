a8ba8384c5144b06c6080f8bd2bb5c9786b10d91
1005106504 <1005106504@L815WD220023.edu.cbe.ab.ca>
2022-09-12 12:57:43 -0600

    12:56

--- Chapter10/src/DivisableBy3.java
+++ Chapter10/src/DivisableBy3.java
@@ -1,4 +1,15 @@
+/*
+ * 
+Program: DivisableBy3.java          Last Date of this Revision: 12-Sep-2022
 
-public class DivisableBy3 {
+Purpose: 
 
+Author: Ashleen Sidhu, 
+School: CHHS
+Course: Computer Programming 20
+ 
+*/
+public class DivisableBy3 
+{
+	
 }
